4fdfe1d965d3b4ef0716c096d8a324185d5ac2b2
veri-sys <139801000+veri-sys@users.noreply.github.com>
2023-07-18 11:58:55 +0500

    Add files via upload

---
+++ bookmarks.js
@@ -0,0 +1,48 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "Links",
+    links: [
+      { name: "About", url: "#about" },
+      { name: "Services", url: "#services" },
+      { name: "News", url: "#news" },
+    ],
+  },
+/*  {
+    title: "Media",
+    links: [
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Netflix", url: "https://netflix.com" },
+      { name: "Crunchyroll", url: "https://crunchyroll.com" },
+      {
+        name: "Amazon Prime",
+        url: "https://www.amazon.com/Amazon-Video",
+      },
+    ],
+  },
+  {
+    title: "Reddit",
+    links: [
+      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
+      {
+        name: "/r/pcmasterrace",
+        url: "https://reddit.com/r/pcmasterrace",
+      },
+      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
+      {
+        name: "/r/battlestations",
+        url: "https://reddit.com/r/battlestations",
+      },
+      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
+      { name: "/r/news", url: "https://reddit.com/r/news" },
+    ],
+  }, */
+  {
+    title: "Contact",
+    links: [
+      { name: "595 Bay St., Toronto ON, M5G 2R3", url: ""},
+      { name: "contact@veri-sys.com", url: "mailto:contact@veri-sys.com" },
+      { name: "+1 437-886-2625", url: "" },
+    ],
+  },
+];
