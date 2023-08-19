34e6dbfbec8999b532eb00f83b1b23e68758b2af
veri-sys <139801000+veri-sys@users.noreply.github.com>
2023-08-19 14:22:47 +0500

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,4 @@
-/*
-Theme: Techro - Free business bootstrap template
-Description: Free to use for personal and commercial use
-Author: WebThemez.com
-Website: http://webthemez.com
-Note: Please do not remove the footer backlink (webthemez.com)--(if you want to remove contact: webthemez@gmail.com)
-Licence: Creative Commons Attribution 3.0** - http://creativecommons.org/licenses/by/3.0/
-*/
+
 jQuery(document).ready(function ($) {
     //da slider
     $('#da-slider').cslider({
